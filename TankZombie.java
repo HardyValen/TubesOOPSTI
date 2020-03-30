510f55ffa1944351066e581c53dc0828c27a723c
ShafaAM <shfamrsya@gmail.com>
2020-03-30 22:31:19 +0700

    add tankzombie

---
+++ TankZombie.java
@@ -0,0 +1,20 @@
+ public class TankZombie extends Zombie{
+ 
+    public TankZombie(int spawnedTurn, Tile tile){
+        super(
+            spawnedTurn,    // Turn Spawned
+            350,             // Max Health
+            -1,             // Direction
+            0x1f9df,        // 🧟‍♂️
+            "Tank Zombie",// Entity Name
+            9,              // Action Time
+            8,              // Attack Damage
+            tile
+        );
+    }
+
+    public void print(){
+        super.print();
+        System.out.println("A ripped zombie");
+    }
+}
