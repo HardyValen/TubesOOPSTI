81e897021446d11e40ae096f5c21c2c9aed36578
HardyValen <hardyvalen@gmail.com>
2020-04-18 03:58:47 +0700

    Stable Build v1.0.1, fixed jar file

--- Constants.java
+++ Constants.java
@@ -1,6 +1,4 @@
 
-import java.net.URL;
-
 public class Constants {
     // Title and JFrame Composition
     public static final String TITLE = "Plants vs Zombies: Bootleg Edition";
@@ -16,6 +14,7 @@ public class Constants {
     public static final String BUTTON_PLAY_GAME_URL = "assets/Buttons/ButtonPlayGame.png";
     public static final String BUTTON_PAUSE_URL = "assets/Buttons/ButtonPause.png";
     public static final String BUTTON_SHOVEL_URL = "assets/Buttons/ButtonShovel.png";
+
     public static final String ENVIRONMENT_CHERRY_BOMB_EXPLOSION_URL = "assets/Environment/CherryBombExplosion.png";
     public static final String ENVIRONMENT_GAMEOVER_URL = "assets/Environment/GameOverOverlay.png";
     public static final String ENVIRONMENT_GAME_SCREEN_URL = "assets/Environment/GameScreen.png";
@@ -23,6 +22,7 @@ public class Constants {
     public static final String ENVIRONMENT_POTATO_MINE_EXPLOSION_URL = "assets/Environment/PotatoMineExplosion.png";
     public static final String ENVIRONMENT_SUN_ICON_URL = "assets/Environment/SunIcon.png";
     public static final String ENVIRONMENT_SUN_LAWN_URL = "assets/Environment/SunLawn.png";
+    
     public static final String PLANTS_CHERRY_BOMB_URL = "assets/Plants/CherryBomb.png";
     public static final String PLANTS_PEASHOOTER_URL = "assets/Plants/Peashooter.png";
     public static final String PLANTS_POTATO_MINE_READY_URL = "assets/Plants/PotatoMine.png";
@@ -32,6 +32,7 @@ public class Constants {
     public static final String PLANTS_SUNFLOWER_URL = "assets/Plants/Sunflower.png";
     public static final String PLANTS_THREEPEATER_URL = "assets/Plants/Threepeater.png";
     public static final String PLANTS_WALLNUT_URL = "assets/Plants/Wallnut.png";
+
     public static final String SEEDPACKETS_CHERRY_BOMB_URL = "assets/SeedPackets/CherryBombSeedPacket.png";
     public static final String SEEDPACKETS_PEASHOOTER_URL = "assets/SeedPackets/PeashooterSeedPacket.png";
     public static final String SEEDPACKETS_POTATO_MINE_URL = "assets/SeedPackets/PotatoMineSeedPacket.png";
@@ -40,11 +41,13 @@ public class Constants {
     public static final String SEEDPACKETS_SUNFLOWER_URL = "assets/SeedPackets/SunflowerSeedPacket.png";
     public static final String SEEDPACKETS_THREEPEATER_URL = "assets/SeedPackets/ThreepeaterSeedPacket.png";
     public static final String SEEDPACKETS_WALLNUT_URL = "assets/SeedPackets/WallnutSeedPacket.png";
+
     public static final String ZOMBIES_NORMAL_URL = "assets/Zombies/NormalZombie.png";
     public static final String ZOMBIES_CONEHEAD_URL = "assets/Zombies/ConeheadZombie.png";
     public static final String ZOMBIES_BUCKETHEAD_URL = "assets/Zombies/BucketheadZombie.png";
     public static final String ZOMBIES_BRICKHEAD_URL = "assets/Zombies/BrickheadZombie.png";
     public static final String ZOMBIES_GARGANTUAR_URL = "assets/Zombies/Gargantuar.png";
+
     public static final String LOGO_URL = "assets/MainScreen/LogoGame.png";
     public static final String MAIN_SCREEN_URL = "assets/MainScreen/MainScreen.png";
 
--- ImageFactory.java
+++ ImageFactory.java
@@ -6,108 +6,108 @@ public class ImageFactory {
 
         switch (image) {
             case BUTTON_PLAY_GAME:
-                imageIcon = new ImageIcon(Constants.BUTTON_PLAY_GAME_URL);
+                imageIcon = new ImageIcon(ImageFactory.class.getResource(Constants.BUTTON_PLAY_GAME_URL));
                 break;
             case BUTTON_PAUSE:
-                imageIcon = new ImageIcon(Constants.BUTTON_PAUSE_URL);
+                imageIcon = new ImageIcon(ImageFactory.class.getResource(Constants.BUTTON_PAUSE_URL));
                 break;
             case BUTTON_SHOVEL:
-                imageIcon = new ImageIcon(Constants.BUTTON_SHOVEL_URL);
+                imageIcon = new ImageIcon(ImageFactory.class.getResource(Constants.BUTTON_SHOVEL_URL));
                 break;
             case ENVIRONMENT_CHERRY_BOMB_EXPLOSION:
-                imageIcon = new ImageIcon(Constants.ENVIRONMENT_CHERRY_BOMB_EXPLOSION_URL);
+                imageIcon = new ImageIcon(ImageFactory.class.getResource(Constants.ENVIRONMENT_CHERRY_BOMB_EXPLOSION_URL));
                 break;
             case ENVIRONMENT_GAMEOVER:
-                imageIcon = new ImageIcon(Constants.ENVIRONMENT_GAMEOVER_URL);
+                imageIcon = new ImageIcon(ImageFactory.class.getResource(Constants.ENVIRONMENT_GAMEOVER_URL));
                 break;
             case ENVIRONMENT_GAME_SCREEN:
-                imageIcon = new ImageIcon(Constants.ENVIRONMENT_GAME_SCREEN_URL);
+                imageIcon = new ImageIcon(ImageFactory.class.getResource(Constants.ENVIRONMENT_GAME_SCREEN_URL));
                 break;
             case ENVIRONMENT_PEA_PROJECTILE:
-                imageIcon = new ImageIcon(Constants.ENVIRONMENT_PEA_PROJECTILE_URL);
+                imageIcon = new ImageIcon(ImageFactory.class.getResource(Constants.ENVIRONMENT_PEA_PROJECTILE_URL));
                 break;
             case ENVIRONMENT_POTATO_MINE_EXPLOSION:
-                imageIcon = new ImageIcon(Constants.ENVIRONMENT_POTATO_MINE_EXPLOSION_URL);
+                imageIcon = new ImageIcon(ImageFactory.class.getResource(Constants.ENVIRONMENT_POTATO_MINE_EXPLOSION_URL));
                 break;
             case ENVIRONMENT_SUN_ICON:
-                imageIcon = new ImageIcon(Constants.ENVIRONMENT_SUN_ICON_URL);
+                imageIcon = new ImageIcon(ImageFactory.class.getResource(Constants.ENVIRONMENT_SUN_ICON_URL));
                 break;
             case ENVIRONMENT_SUN_LAWN:
-                imageIcon = new ImageIcon(Constants.ENVIRONMENT_SUN_LAWN_URL);
+                imageIcon = new ImageIcon(ImageFactory.class.getResource(Constants.ENVIRONMENT_SUN_LAWN_URL));
                 break;
             case PLANTS_CHERRY_BOMB:
-                imageIcon = new ImageIcon(Constants.PLANTS_CHERRY_BOMB_URL);
+                imageIcon = new ImageIcon(ImageFactory.class.getResource(Constants.PLANTS_CHERRY_BOMB_URL));
                 break;
             case PLANTS_PEASHOOTER:
-                imageIcon = new ImageIcon(Constants.PLANTS_PEASHOOTER_URL);
+                imageIcon = new ImageIcon(ImageFactory.class.getResource(Constants.PLANTS_PEASHOOTER_URL));
                 break;
             case PLANTS_POTATO_MINE_READY:
-                imageIcon = new ImageIcon(Constants.PLANTS_POTATO_MINE_READY_URL);
+                imageIcon = new ImageIcon(ImageFactory.class.getResource(Constants.PLANTS_POTATO_MINE_READY_URL));
                 break;
             case PLANTS_POTATO_MINE_UNARMED:
-                imageIcon = new ImageIcon(Constants.PLANTS_POTATO_MINE_UNARMED_URL);
+                imageIcon = new ImageIcon(ImageFactory.class.getResource(Constants.PLANTS_POTATO_MINE_UNARMED_URL));
                 break;
             case PLANTS_REPEATER:
-                imageIcon = new ImageIcon(Constants.PLANTS_REPEATER_URL);
+                imageIcon = new ImageIcon(ImageFactory.class.getResource(Constants.PLANTS_REPEATER_URL));
                 break;
             case PLANTS_SPIKEWEED:
-                imageIcon = new ImageIcon(Constants.PLANTS_SPIKEWEED_URL);
+                imageIcon = new ImageIcon(ImageFactory.class.getResource(Constants.PLANTS_SPIKEWEED_URL));
                 break;
             case PLANTS_SUNFLOWER:
-                imageIcon = new ImageIcon(Constants.PLANTS_SUNFLOWER_URL);
+                imageIcon = new ImageIcon(ImageFactory.class.getResource(Constants.PLANTS_SUNFLOWER_URL));
                 break;
             case PLANTS_THREEPEATER:
-                imageIcon = new ImageIcon(Constants.PLANTS_THREEPEATER_URL);
+                imageIcon = new ImageIcon(ImageFactory.class.getResource(Constants.PLANTS_THREEPEATER_URL));
                 break;
             case PLANTS_WALLNUT:
-                imageIcon = new ImageIcon(Constants.PLANTS_WALLNUT_URL);
+                imageIcon = new ImageIcon(ImageFactory.class.getResource(Constants.PLANTS_WALLNUT_URL));
                 break;
 
             case SEEDPACKETS_CHERRY_BOMB:
-                imageIcon = new ImageIcon(Constants.SEEDPACKETS_CHERRY_BOMB_URL);
+                imageIcon = new ImageIcon(ImageFactory.class.getResource(Constants.SEEDPACKETS_CHERRY_BOMB_URL));
                 break;
             case SEEDPACKETS_PEASHOOTER:
-                imageIcon = new ImageIcon(Constants.SEEDPACKETS_PEASHOOTER_URL);
+                imageIcon = new ImageIcon(ImageFactory.class.getResource(Constants.SEEDPACKETS_PEASHOOTER_URL));
                 break;
             case SEEDPACKETS_POTATO_MINE:
-                imageIcon = new ImageIcon(Constants.SEEDPACKETS_POTATO_MINE_URL);
+                imageIcon = new ImageIcon(ImageFactory.class.getResource(Constants.SEEDPACKETS_POTATO_MINE_URL));
                 break;
             case SEEDPACKETS_REPEATER:
-                imageIcon = new ImageIcon(Constants.SEEDPACKETS_REPEATER_URL);
+                imageIcon = new ImageIcon(ImageFactory.class.getResource(Constants.SEEDPACKETS_REPEATER_URL));
                 break;
             case SEEDPACKETS_SPIKEWEED:
-                imageIcon = new ImageIcon(Constants.SEEDPACKETS_SPIKEWEED_URL);
+                imageIcon = new ImageIcon(ImageFactory.class.getResource(Constants.SEEDPACKETS_SPIKEWEED_URL));
                 break;
             case SEEDPACKETS_SUNFLOWER:
-                imageIcon = new ImageIcon(Constants.SEEDPACKETS_SUNFLOWER_URL);
+                imageIcon = new ImageIcon(ImageFactory.class.getResource(Constants.SEEDPACKETS_SUNFLOWER_URL));
                 break;
             case SEEDPACKETS_THREEPEATER:
-                imageIcon = new ImageIcon(Constants.SEEDPACKETS_THREEPEATER_URL);
+                imageIcon = new ImageIcon(ImageFactory.class.getResource(Constants.SEEDPACKETS_THREEPEATER_URL));
                 break;
             case SEEDPACKETS_WALLNUT:
-                imageIcon = new ImageIcon(Constants.SEEDPACKETS_WALLNUT_URL);
+                imageIcon = new ImageIcon(ImageFactory.class.getResource(Constants.SEEDPACKETS_WALLNUT_URL));
                 break;
 
             case ZOMBIES_NORMAL:
-                imageIcon = new ImageIcon(Constants.ZOMBIES_NORMAL_URL);
+                imageIcon = new ImageIcon(ImageFactory.class.getResource(Constants.ZOMBIES_NORMAL_URL));
                 break;
             case ZOMBIES_CONEHEAD:
-                imageIcon = new ImageIcon(Constants.ZOMBIES_CONEHEAD_URL);
+                imageIcon = new ImageIcon(ImageFactory.class.getResource(Constants.ZOMBIES_CONEHEAD_URL));
                 break;
             case ZOMBIES_BUCKETHEAD:
-                imageIcon = new ImageIcon(Constants.ZOMBIES_BUCKETHEAD_URL);
+                imageIcon = new ImageIcon(ImageFactory.class.getResource(Constants.ZOMBIES_BUCKETHEAD_URL));
                 break;
             case ZOMBIES_BRICKHEAD:
-                imageIcon = new ImageIcon(Constants.ZOMBIES_BRICKHEAD_URL);
+                imageIcon = new ImageIcon(ImageFactory.class.getResource(Constants.ZOMBIES_BRICKHEAD_URL));
                 break;
             case ZOMBIES_GARGANTUAR:
-                imageIcon = new ImageIcon(Constants.ZOMBIES_GARGANTUAR_URL);
+                imageIcon = new ImageIcon(ImageFactory.class.getResource(Constants.ZOMBIES_GARGANTUAR_URL));
                 break;
             case LOGO:
-                imageIcon = new ImageIcon(Constants.LOGO_URL);
+                imageIcon = new ImageIcon(ImageFactory.class.getResource(Constants.LOGO_URL));
                 break;
             case MAIN_SCREEN:
-                imageIcon = new ImageIcon(Constants.MAIN_SCREEN_URL);
+                imageIcon = new ImageIcon(ImageFactory.class.getResource(Constants.MAIN_SCREEN_URL));
                 break;
             default:
                 return null;
